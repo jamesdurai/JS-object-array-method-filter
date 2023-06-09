91f13ccfb4cde111c1cdaf95e08ffffc9b41b7bb
jamesdurai <jamesdurai@outlook.com>
2023-06-09 10:17:49 +0530

    Objecr array filter method

---
+++ main.js
@@ -0,0 +1,15 @@
+const students = [
+  { name: 'Quincy', grade: 96 },
+  { name: 'Jason', grade: 84 },
+  { name: 'Alexis', grade: 100 },
+  { name: 'Sam', grade: 65 },
+  { name: 'Katie', grade: 90 }
+];
+
+// Output:[ { name: 'Quincy', grade: 96 }, { name: 'Alexis', grade: 100 }, { name: 'Katie', grade: 90 } ]
+
+let result=students.filter(function(e){
+	
+  return e.grade>=90
+});
+document.write(JSON.stringify(result))
